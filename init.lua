0b30324842dbdee11eefde08ed22534525a16284
Eumir Gaspar <corroded@users.noreply.github.com>
2023-12-12 09:07:26 +0800

    Added Neotree

--- init.lua
+++ init.lua
@@ -245,6 +245,18 @@ require('lazy').setup({
     },
   },
 
+  -- Neo tree 
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  },
+
   {
     -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
