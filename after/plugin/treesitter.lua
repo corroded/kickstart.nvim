2b0d9840372bc4cd0c985cfbfba601a1c606c36c
Eumir Gaspar <corroded@users.noreply.github.com>
2024-11-20 18:10:49 +0800

    Copy treesitter from terence

---
+++ after/plugin/treesitter.lua
@@ -0,0 +1,29 @@
+vim.defer_fn(function()
+  require('nvim-treesitter.configs').setup {
+    -- Add languages to be installed here that you want installed for treesitter
+    ensure_installed = {
+      'c',
+      'cpp',
+      'go',
+      'lua',
+      'python',
+      'rust',
+      'tsx',
+      'javascript',
+      'typescript',
+      'vimdoc',
+      'vim',
+      'bash',
+      'ruby',
+      'erlang',
+      'elixir',
+      'eex',
+      'heex',
+      'html',
+      'css',
+      'markdown',
+      'markdown_inline',
+      'terraform',
+    },
+  }
+end, 0)
