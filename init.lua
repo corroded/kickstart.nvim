f113925784faf357dd72b045580a5217c228853d
Eumir Gaspar <corroded@users.noreply.github.com>
2024-11-20 18:11:03 +0800

    Fixed neo-tree setup
    Used config = function over opts mainly to actually run the
    setting. opts assumes it was installed via Lazy

--- init.lua
+++ init.lua
@@ -891,43 +891,13 @@ require('lazy').setup({
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
-  {
-    'nvim-neo-tree/neo-tree.nvim',
-    branch = 'v3.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-      'MunifTanjim/nui.nvim',
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-  },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = {
-        'bash',
-        'c',
-        'cpp',
-        'css',
-        'eex',
-        'elixir',
-        'erlang',
-        'go',
-        'heex',
-        'html',
-        'javascript',
-        'lua',
-        'python',
-        'ruby',
-        'rust',
-        'tsx',
-        'typescript',
-        'vim',
-        'vimdoc',
-      },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -9,17 +9,26 @@ return {
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
-  cmd = 'Neotree',
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-  },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
+  config = function()
+    require('neo-tree').setup {
+      cmd = 'Neotree',
+      keys = {
+        { '\\', ':Neotree toggle<CR>', desc = 'NeoTree reveal', silent = true },
+      },
+      close_if_last_window = true,
+      filesystem = {
+        hijack_netrw_behavior = 'open_default',
+        window = {
+          mappings = {
+            ['\\'] = 'close_window',
+          },
         },
       },
-    },
-  },
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+      },
+    }
+  end,
 }
