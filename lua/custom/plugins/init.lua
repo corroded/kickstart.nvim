5eade053a32d809d9f95edcf9366c6132794e926
Eumir Gaspar <corroded@users.noreply.github.com>
2024-11-30 04:39:17 +0800

    Added vv for vertical split

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,9 @@
 -- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
+-- I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+-- Key mapping for vertical split
+vim.keymap.set('n', 'vv', ':vsplit<CR>', { noremap = true, silent = true })
+
 return {}
